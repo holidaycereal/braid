463b489830245ae538ffecf9c10fe80c43f7be6d
Alena Weiley <alena@weiley.org>
2025-01-15 15:00:11 +1100

    code style

--- lexer/token_type.h
+++ lexer/token_type.h
@@ -3,32 +3,92 @@
 
 typedef enum {
 	// Keywords
-	WORD_IF, WORD_THEN, WORD_ELSE, WORD_WHILE, WORD_FOR, WORD_IN, WORD_BREAK,
-	WORD_CONTINUE, WORD_MATCH, WORD_DYN, WORD_DATA, WORD_TYPE, WORD_INCLUDE,
-	WORD_IMPORT, WORD_EXIT,
+	WORD_IF,
+	WORD_THEN,
+	WORD_ELSE,
+	WORD_WHILE,
+	WORD_FOR,
+	WORD_IN,
+	WORD_BREAK,
+	WORD_CONTINUE,
+	WORD_MATCH,
+	WORD_DYN,
+	WORD_DATA,
+	WORD_TYPE,
+	WORD_INCLUDE,
+	WORD_IMPORT,
+	WORD_EXIT,
 
 	// Primitives
-	PRIM_U8, PRIM_U16, PRIM_U32, PRIM_U64, PRIM_I8, PRIM_I16, PRIM_I32,
-	PRIM_I64, PRIM_USIZE, PRIM_ISIZE, PRIM_F32, PRIM_F64, PRIM_BOOL,
+	PRIM_U8,
+	PRIM_U16,
+	PRIM_U32,
+	PRIM_U64,
+	PRIM_I8,
+	PRIM_I16,
+	PRIM_I32,
+	PRIM_I64,
+	PRIM_USIZE,
+	PRIM_ISIZE,
+	PRIM_F32,
+	PRIM_F64,
+	PRIM_BOOL,
 
 	// Boolean literals
-	LIT_TRUE, LIT_FALSE,
+	LIT_TRUE,
+	LIT_FALSE,
 
 	// Tokens with a string value
-	IDENT, LIT_NUM, LIT_CHAR, LIT_STR, LIT_STR_RAW, ERR_TOKEN,
+	IDENT,
+	LIT_NUM,
+	LIT_CHAR,
+	LIT_STR,
+	LIT_STR_RAW,
+	ERR_TOKEN,
 
 	// Punctuation & operators
-	PAREN_L, PAREN_R, BRACKET_L, BRACKET_R, BRACE_L, BRACE_R, DOT, COMMA,
-	SEMICOLON, COLON, EQUALS, VERT_LINE, DOLLAR, AMPERSAND, BANG, QUESTION,
-	HASH, CARET, MINUS, PLUS, STAR, SLASH, PERCENT, LESS, GREATER,
-	TEST_EQ, NOT_EQ,            //  ==  !=
-	LESS_EQ, GREATER_EQ,        //  <=  >=
-	AND, OR, XOR,               //  &&  ||  ##
-	ARROW,                      //  ->
-	PLUS_EQUALS, MINUS_EQUALS,  //  +=  -=  *=  /=  ^=  %=
-	STAR_EQUALS, SLASH_EQUALS, CARET_EQUALS, PERCENT_EQUALS,
-	RANGE,                      //  ..
-	MODULE,                     //  ::
+	PAREN_L,
+	PAREN_R,
+	BRACKET_L,
+	BRACKET_R,
+	BRACE_L,
+	BRACE_R,
+	DOT,
+	COMMA,
+	SEMICOLON,
+	COLON,
+	EQUALS,
+	VERT_LINE,
+	DOLLAR,
+	AMPERSAND,
+	BANG,
+	QUESTION,
+	HASH,
+	CARET,
+	MINUS,
+	PLUS,
+	STAR,
+	SLASH,
+	PERCENT,
+	LESS,
+	GREATER,
+	// Multi-character operators
+	TEST_EQ,         //  ==
+	NOT_EQ,          //  !=
+	LESS_EQ,         //  <=
+	GREATER_EQ,      //  >=
+	AND,             //  &&
+	OR,              //  ||
+	XOR,             //  ##
+	ARROW,           //  ->
+	PLUS_EQUALS,     //  +=
+	MINUS_EQUALS,    //  -=
+	STAR_EQUALS,     //  *=
+	SLASH_EQUALS,    //  /=
+	CARET_EQUALS,    //  ^=
+	PERCENT_EQUALS,  //  %=
+	RANGE,           //  ..
+	MODULE,          //  ::
 
 	EOF_TOKEN,  // End of input
 } TokenType;
