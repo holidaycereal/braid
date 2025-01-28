324c31e5dec4e0f07a8faa28c448cd6234903eb8
Alena Weiley <alena@weiley.org>
2025-01-28 16:24:46 +1100

    start work on parser

---
+++ parser/parser.ml
@@ -0,0 +1,11 @@
+open Lexer
+open Stdint
+
+type num_kind = Signed | Unsigned | Floating
+type width = W8 | W16 | W32 | W64
+
+type terminal =
+  | NumVal of (uint64 * num_kind * width)
+  | BoolVal of bool
+  | CharVal of uint32
+  | StringVal of (string * int)
